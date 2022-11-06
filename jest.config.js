e8fdd6433050f4c969d2ce9f078b61f35809f99c
Jessy BOGALHO <j.bogalho@m2mfactory.fr>
2022-11-06 11:36:17 +0100

    first commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.mts',
+    '!src/**/*.d.ts',
+    '!src/**/*.d.mts',
+  ],
+};
